9592cc791c45d88c930df3c4021a31630b20ca7f
Jairo Diaz <jairo.diaz@codescrum.com>
2013-09-14 16:26:28 +0100

    Cleaned code for upload from different bucket names

--- app/assets/javascripts/avatar.js
+++ app/assets/javascripts/avatar.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
     var options = {
-        //target:        '#user_key',   // target element(s) to be updated with server response
-        //beforeSubmit:  showRequest,  // pre-submit callback
+        beforeSubmit:  showRequest,  // pre-submit callback
         success:       showResponse,  // post-submit callback
-
-        // other available options:
-        //url:       url         // override for form's 'action' attribute
-        //type:      type        // 'get' or 'post', override for form's 'method' attribute
-        //dataType:  'xml',        // 'xml', 'script', or 'json' (expected server response type)
-        //clearForm: true        // clear all form fields after successful submit
-        //resetForm: true        // reset the form after successful submit
-
-        // $.ajax options can be used here too, for example:
-        //timeout:   3000
     };
 
-      // bind form using 'ajaxForm'
-      // bind 'myForm' and provide a simple callback function
-      $('#new_avatar_uploader').ajaxForm(options);
+    $('#new_avatar_uploader').ajaxForm(options);
 
     $('#new_avatar_uploader').change(function() {
       $('#new_avatar_uploader').submit();
@@ -25,22 +12,19 @@ $(document).ready(function() {
 
   });
 
+  // pre-submit callback
+  function showRequest(formData, jqForm, options) {
+    $("#user_key").val("Loading file in the background, please wait...");
+    return true;
+  }
+
   // post-submit callback
   function showResponse(responseText, statusText, xhr, $form)  {
       // This Header need to be exposed on the CORS configuration in Amazon.
       var location = xhr.getResponseHeader('Location');
+      key_position = location.indexOf("uploads");
+      key = location.substring(key_position, location.length);
+      key = key.replace(/%2F/g,"/");
 
-      $("#user_key").val(location);
-      // for normal html responses, the first argument to the success callback
-      // is the XMLHttpRequest object's responseText property
-
-      // if the ajaxForm method was passed an Options Object with the dataType
-      // property set to 'xml' then the first argument to the success callback
-      // is the XMLHttpRequest object's responseXML property
-
-      // if the ajaxForm method was passed an Options Object with the dataType
-      // property set to 'json' then the first argument to the success callback
-      // is the json data object returned by the server
-      //alert('status: ' + statusText + '\n\nresponseText: \n' + responseText +
-      //    '\n\nThe output div should have already been updated with the responseText.');
+      $("#user_key").val(key);
   }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new(params)
     @uploader = User.new.avatar
-    #This needs empty!
-    @uploader.success_action_redirect = ""#new_avatar_url
+    @uploader.success_action_redirect = "" #This needs to be empty! Nill will produce an error in Amazon S3
   end
 
   def create
     @user = User.new(params[:user])
-    #binding.pry
     if @user.save
+      #To process thumbnails this has to be executed in a background job:
       #@user.remote_avatar_url = @user.avatar.direct_fog_url(:with_path => true)
       redirect_to @user, notice: 'User was successfully created. Waiting for Avatar to be processed. Refresh at will'
     else
