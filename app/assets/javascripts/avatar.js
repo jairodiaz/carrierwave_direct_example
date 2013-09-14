83c6f19d5bbf358759be4537b603d2ab49319d2e
Jairo Diaz <jairo.diaz@codescrum.com>
2013-09-14 14:33:47 +0100

    Configured retrieval of Location Header

--- app/assets/javascripts/avatar.js
+++ app/assets/javascripts/avatar.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     var options = {
         //target:        '#user_key',   // target element(s) to be updated with server response
         //beforeSubmit:  showRequest,  // pre-submit callback
-        success:       showResponse  // post-submit callback
+        success:       showResponse,  // post-submit callback
 
         // other available options:
         //url:       url         // override for form's 'action' attribute
         //type:      type        // 'get' or 'post', override for form's 'method' attribute
-        //dataType:  null        // 'xml', 'script', or 'json' (expected server response type)
+        //dataType:  'xml',        // 'xml', 'script', or 'json' (expected server response type)
         //clearForm: true        // clear all form fields after successful submit
         //resetForm: true        // reset the form after successful submit
 
@@ -27,21 +27,10 @@ $(document).ready(function() {
 
   // post-submit callback
   function showResponse(responseText, statusText, xhr, $form)  {
-      console.debug("I am here 2");
-      console.debug(xhr);
+      // This Header need to be exposed on the CORS configuration in Amazon.
+      var location = xhr.getResponseHeader('Location');
 
-      file_key = $("#user_key").val();
-      file_name = $("#avatar_uploader_avatar").val();
-      complete_key = file_key.replace("${filename}",file_name);
-
-      console.debug(file_key)
-      console.debug(file_name)
-      console.debug(complete_key)
-
-      $("#user_key").val(complete_key);
-      //window.location.href = data.redirect
-      //http://stackoverflow.com/questions/199099/how-to-manage-a-redirect-request-after-a-jquery-ajax-call
-
+      $("#user_key").val(location);
       // for normal html responses, the first argument to the success callback
       // is the XMLHttpRequest object's responseText property
 
@@ -52,6 +41,6 @@ $(document).ready(function() {
       // if the ajaxForm method was passed an Options Object with the dataType
       // property set to 'json' then the first argument to the success callback
       // is the json data object returned by the server
-      alert('status: ' + statusText + '\n\nresponseText: \n' + responseText +
-          '\n\nThe output div should have already been updated with the responseText.');
+      //alert('status: ' + statusText + '\n\nresponseText: \n' + responseText +
+      //    '\n\nThe output div should have already been updated with the responseText.');
   }
